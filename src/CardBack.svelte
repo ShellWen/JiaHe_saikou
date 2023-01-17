62a1644ca206a652205b1f4e926df3541083bbbd
ShellWen <gofly233@gmail.com>
2023-01-17 18:42:31 +0800

    :sparkles: 移除卡背社交方式样板代码

--- src/CardBack.svelte
+++ src/CardBack.svelte
@@ -1,47 +1,35 @@
-<script lang="ts">
+<script lang='ts'>
   import Icon from 'svelte-awesome'
-  import { weibo, qq, twitter, telegram, steam, github, envelope } from 'svelte-awesome/icons'
-  import { faTiktok as tiktok, faBilibili as bilibili } from '@fortawesome/free-brands-svg-icons';
+  import { envelope, github, qq, steam, telegram, twitter, weibo } from 'svelte-awesome/icons'
+  import { faBilibili as bilibili, faTiktok as tiktok } from '@fortawesome/free-brands-svg-icons'
+
+  interface Social {
+    text: string;
+    icon: any;
+    link: string;
+  }
+
+  const socials: Social[] = [
+    { text: 'JiaHe005', icon: tiktok, link: 'https://v.douyin.com/Lox8duD/' },
+    { text: '2093702861', icon: qq, link: 'https://qm.qq.com/cgi-bin/qm/qr?k=E359EoW9MSoSOuFMmSteLUjlqFQZFxgs' },
+    { text: '嘉何今天没睡醒', icon: weibo, link: 'https://weibo.com/u/6482679368' },
+    { text: '嘉何今天没睡醒', icon: bilibili, link: 'https://space.bilibili.com/321199489' },
+    { text: '@JiaHe888', icon: twitter, link: 'https://twitter.com/JiaHe888' },
+    { text: '@jiahe_furry', icon: telegram, link: 'https://t.me/jiahe_furry' },
+    { text: 'jiahe005', icon: steam, link: 'https://steamcommunity.com/id/jiahe005/' },
+    { text: '@jiahe005', icon: github, link: 'https://github.com/jiahe005' },
+    { text: 'jiahe005@petalmail.com', icon: envelope, link: 'mailto:jiahe005@petalmail.com' }
+  ]
 </script>
 
 <div class='w-full h-[400px] flex-1 flex flex-col justify-between p-4'>
   <div class='flex flex-col'>
-    <a href='https://v.douyin.com/Lox8duD/' target='_blank' rel='noopener noreferrer' class='link'>
-      <Icon data={tiktok} class='inline-block' />
-      JiaHe005
-    </a>
-    <a href='https://qm.qq.com/cgi-bin/qm/qr?k=E359EoW9MSoSOuFMmSteLUjlqFQZFxgs' target='_blank' rel='noopener noreferrer' class='link'>
-      <Icon data={qq} class='inline-block' />
-      2093702861
-    </a>
-    <a href='https://weibo.com/u/6482679368' target='_blank' rel='noopener noreferrer' class='link'>
-      <Icon data={weibo} class='inline-block' />
-      嘉何今天没睡醒
-    </a>
-    <a href='https://space.bilibili.com/321199489' target='_blank' rel='noopener noreferrer' class='link'>
-      <Icon data={bilibili} class='inline-block' />
-      嘉何今天没睡醒
-    </a>
-    <a href='https://twitter.com/JiaHe888' target='_blank' rel='noopener noreferrer' class='link'>
-      <Icon data={twitter} class='inline-block' />
-      @JiaHe888
-    </a>
-    <a href='https://t.me/jiahe_furry' target='_blank' rel='noopener noreferrer' class='link'>
-      <Icon data={telegram} class='inline-block' />
-      @jiahe_furry
-    </a>
-    <a href='https://steamcommunity.com/id/jiahe005/' target='_blank' rel='noopener noreferrer' class='link'>
-      <Icon data={steam} class='inline-block' />
-      jiahe005
-    </a>
-    <a href='https://github.com/jiahe005' target='_blank' rel='noopener noreferrer' class='link'>
-      <Icon data={github} class='inline-block' />
-      @jiahe005
-    </a>
-    <a href='mailto:jiahe005@petalmail.com' target='_blank' rel='noopener noreferrer' class='link'>
-      <Icon data={envelope} class='inline-block' />
-      jiahe005@petalmail.com
-    </a>
+    {#each socials as social}
+      <a class='link' href={social.link} rel='noopener noreferrer' target='_blank'>
+        <Icon class='inline-block' data={social.icon} />
+        {social.text}
+      </a>
+    {/each}
   </div>
   <div class='relative'>
     <img alt='' class='absolute bottom-0 right-0 translate-y-[45px] translate-x-[15px] w-32'
